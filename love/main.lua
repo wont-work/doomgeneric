08b28aba91152288a2e67647bc8e004c445e4165
Kopper <kopper@w.on-t.work>
2024-12-22 22:54:12 +0300

    handle resize

--- love/main.lua
+++ love/main.lua
@@ -3,20 +3,24 @@ local doom = require 'doom'
 local band = bit.band;
 local rshift = bit.rshift;
 
-local w = doom.get_width()
-local h = doom.get_height()
-local canvas = love.image.newImageData(w + 1, h + 1, "rgba8")
+local x, y = 0, 0;
+local doomW = doom.get_width()
+local doomH = doom.get_height()
+local canvas = love.image.newImageData(doomW, doomH, "rgba8")
 local image = love.graphics.newImage(canvas)
 
 function love.load()
+    local _, _, w, h = love.window.getSafeArea()
+    love.resize(w, h)
+
     doom.start()
 end
 
 function love.draw()
     if not doom.get_want_redraw() then return end
 
-    for x = 0, w do
-        for y = 0, h do
+    for x = 0, doomW - 1 do
+        for y = 0, doomH - 1 do
             local bgra = doom.get_pixel_at(x, y)
             local b = band(bgra, 0xff) / 255
             local g = band(rshift(bgra, 8), 0xff) / 255
@@ -26,7 +30,7 @@ function love.draw()
     end
 
     image:replacePixels(canvas)
-    love.graphics.draw(image, 0, 0)
+    love.graphics.draw(image, x, y)
 end
 
 function love.update(dt)
@@ -39,4 +43,9 @@ end
 
 function love.keyreleased(key)
     doom.release_key(key)
+end
+
+function love.resize(winW, winH)
+    x = (winW - doomW) / 2
+    y = (winH - doomH) / 2
 end
